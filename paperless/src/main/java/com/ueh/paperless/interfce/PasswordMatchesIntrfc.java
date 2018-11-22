b37466b84b380e6ec9e0449af796407c659afa97
Georgy Louis <louis.georgy@gmail.com>
2018-11-21 20:24:58 -0500

    Update PasswordMatchesIntrfc.java

--- paperless/src/main/java/com/ueh/paperless/interfce/PasswordMatchesIntrfc.java
+++ paperless/src/main/java/com/ueh/paperless/interfce/PasswordMatchesIntrfc.java
@@ -1,6 +1,6 @@
-//package com.ueh.paperless.interfce;
-//
-//public interface PasswordMatchesIntrfc {
-//	public String getPassword();
-//	public String getConfirmPassword();
-//}
+package com.ueh.paperless.interfce;
+
+public interface PasswordMatchesIntrfc {
+	public String getPassword();
+	public String getConfirmPassword();
+}
