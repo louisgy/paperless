ef0089a541472dc0bcd9b7106fffa5bd56c15608
Georgy Louis <louis.georgy@gmail.com>
2018-11-21 22:28:54 -0500

    Update PasswordMatchesValidator.java

--- paperless/src/main/java/com/ueh/paperless/validator/PasswordMatchesValidator.java
+++ paperless/src/main/java/com/ueh/paperless/validator/PasswordMatchesValidator.java
@@ -1,19 +1,19 @@
-//package com.ueh.paperless.validator;
-//
-//import javax.validation.ConstraintValidator;
-//import javax.validation.ConstraintValidatorContext;
-//
-////import com.ueh.paperless.dto.Studentsdto;
-//import com.ueh.paperless.interfce.PasswordMatchesIntrfc;
-//
-//
-//
-////public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object>{
-//	public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, PasswordMatchesIntrfc>{
-//	@Override
-//	public boolean isValid (PasswordMatchesIntrfc obj, ConstraintValidatorContext context){
-//		//Studentsdto students = (Studentsdto) obj;
-//		PasswordMatchesIntrfc students =(PasswordMatchesIntrfc) obj;
-//	    return students.getPassword().equals(students.getConfirmPassword());
-//	    }
-//}
+package com.ueh.paperless.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.ueh.paperless.dto.Studentsdto;
+import com.ueh.paperless.interfce.PasswordMatchesIntrfc;
+
+
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object>{
+	public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, PasswordMatchesIntrfc>{
+	@Override
+	public boolean isValid (PasswordMatchesIntrfc obj, ConstraintValidatorContext context){
+		//Studentsdto students = (Studentsdto) obj;
+		PasswordMatchesIntrfc students =(PasswordMatchesIntrfc) obj;
+	    return students.getPassword().equals(students.getConfirmPassword());
+	    }
+}
