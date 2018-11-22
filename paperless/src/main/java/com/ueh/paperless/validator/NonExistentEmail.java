4027affc4451a59c07d47474513a0cf3db5b4c95
Georgy Louis <louis.georgy@gmail.com>
2018-11-22 08:01:28 -0500

    Update NonExistentEmail.java

--- paperless/src/main/java/com/ueh/paperless/validator/NonExistentEmail.java
+++ paperless/src/main/java/com/ueh/paperless/validator/NonExistentEmail.java
@@ -1,19 +1,19 @@
-//package com.ueh.paperless.validator;
-//
-//import static java.lang.annotation.ElementType.FIELD;
-//import static java.lang.annotation.RetentionPolicy.RUNTIME;
-//
-//import java.lang.annotation.Retention;
-//import java.lang.annotation.Target;
-//
-//import javax.validation.Constraint;
-//import javax.validation.Payload;
-//
-//@Target(FIELD)
-//@Retention(RUNTIME)
-//@Constraint(validatedBy = NonExistentEmailValidator.class)
-//public @interface NonExistentEmail {
-//	 String message() default "This email is not in our system";
-//	    Class<?>[] groups() default { };
-//	    Class<? extends Payload>[] payload() default { };
-//}
+package com.ueh.paperless.validator;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target(FIELD)
+@Retention(RUNTIME)
+@Constraint(validatedBy = NonExistentEmailValidator.class)
+public @interface NonExistentEmail {
+	 String message() default "This email is not in our system";
+	    Class<?>[] groups() default { };
+	    Class<? extends Payload>[] payload() default { };
+}
