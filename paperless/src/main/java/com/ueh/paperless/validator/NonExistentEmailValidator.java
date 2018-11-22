4cad57c41c473bf6ac4dac37c82b87c82be174c1
Georgy Louis <louis.georgy@gmail.com>
2018-11-21 20:01:13 -0500

    Update NonExistentEmailValidator.java

--- paperless/src/main/java/com/ueh/paperless/validator/NonExistentEmailValidator.java
+++ paperless/src/main/java/com/ueh/paperless/validator/NonExistentEmailValidator.java
@@ -1,27 +1,27 @@
-//package com.ueh.paperless.validator;
-//
-//import javax.validation.ConstraintValidator;
-//import javax.validation.ConstraintValidatorContext;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import com.ueh.paperless.StudentsRepository;
-//import com.ueh.paperless.domain.Students;
-//
-//public class NonExistentEmailValidator implements ConstraintValidator<NonExistentEmail, String> {
-//	@Autowired
-//	StudentsRepository studentsRepository;
-//	
-//	public boolean isValid(String email, ConstraintValidatorContext arg1) {
-//		return EmailExist(email);
-//	}
-//	
-//	public boolean EmailExist(String email) {
-//		Students students= studentsRepository.findByEmail(email);
-//		if(students != null)
-//			return true;
-//		else
-//			return false;
-//	}
-//
-//}
+package com.ueh.paperless.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ueh.paperless.StudentsRepository;
+import com.ueh.paperless.domain.Students;
+
+public class NonExistentEmailValidator implements ConstraintValidator<NonExistentEmail, String> {
+	@Autowired
+	StudentsRepository studentsRepository;
+	
+	public boolean isValid(String email, ConstraintValidatorContext arg1) {
+		return EmailExist(email);
+	}
+	
+	public boolean EmailExist(String email) {
+		Students students= studentsRepository.findByEmail(email);
+		if(students != null)
+			return true;
+		else
+			return false;
+	}
+
+}
