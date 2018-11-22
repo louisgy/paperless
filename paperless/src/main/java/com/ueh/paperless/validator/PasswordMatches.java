14881baa2a27fa86cd6bda51906c8682a4383b52
Georgy Louis <louis.georgy@gmail.com>
2018-11-22 08:00:38 -0500

    Update PasswordMatches.java

--- paperless/src/main/java/com/ueh/paperless/validator/PasswordMatches.java
+++ paperless/src/main/java/com/ueh/paperless/validator/PasswordMatches.java
@@ -1,22 +1,22 @@
-//package com.ueh.paperless.validator;
-//
-//import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
-//import static java.lang.annotation.ElementType.TYPE;
-//import static java.lang.annotation.RetentionPolicy.RUNTIME;
-//
-//import java.lang.annotation.Documented;
-//import java.lang.annotation.Retention;
-//import java.lang.annotation.Target;
-//
-//import javax.validation.Constraint;
-//import javax.validation.Payload;
-//
-//@Documented
-//@Retention(RUNTIME)
-//@Target({ TYPE, ANNOTATION_TYPE })
-//@Constraint(validatedBy = PasswordMatchesValidator.class)
-//public @interface PasswordMatches {
-//	String message() default "Passwords don't match";
-//    Class<?>[] groups() default {};
-//    Class<? extends Payload>[] payload() default {};
-//}
+package com.ueh.paperless.validator;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Documented
+@Retention(RUNTIME)
+@Target({ TYPE, ANNOTATION_TYPE })
+@Constraint(validatedBy = PasswordMatchesValidator.class)
+public @interface PasswordMatches {
+	String message() default "Passwords don't match";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
